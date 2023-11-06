bfd6dcb4fda87b0f3f6a7ad5aebdd7625c2e61f0
Javier <jmartin@grupoloyal.com>
2023-11-06 13:53:24 +0100

    Modulos

--- curso/src/app/app.module.ts
+++ curso/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MyCoreModule } from 'src/lib/my-core';
+import { MainModule } from './main';
+import { SecurityModule } from './security';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule, MyCoreModule, MainModule, SecurityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
